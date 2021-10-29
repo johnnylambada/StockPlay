6848085cb5d11aeb7abd85f79dc52ad550f1c26b
John Lombardo <john@hausinc.net>
2021-10-29 13:16:48 -0700

    Remove Repository

--- app/src/main/java/com/antarikshc/stockplay/ui/MainViewModel.kt
+++ app/src/main/java/com/antarikshc/stockplay/ui/MainViewModel.kt
@@ -4,22 +4,30 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.antarikshc.stockplay.data.Repository
+import com.antarikshc.stockplay.data.local.StockDatabase
+import com.antarikshc.stockplay.data.remote.StockService
+import com.antarikshc.stockplay.data.remote.StockService.Companion.URL
+import com.antarikshc.stockplay.models.IncPrices
 import com.antarikshc.stockplay.models.Stock
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlin.math.round
 
 class MainViewModel @ViewModelInject constructor(
-    private val repository: Repository
+    private val service: StockService,
+    private val db: StockDatabase,
 ) : ViewModel() {
 
     private val _stocks = MutableLiveData<List<Stock>>()
     val stocks = _stocks
 
     init {
-        repository.getStocks().onEach { _stocks.postValue(it) }.launchIn(viewModelScope)
-
-        repository.refreshStocks(viewModelScope)
+        db.stockDao().getStocks().onEach { _stocks.postValue(it) }.launchIn(viewModelScope)
+        service.getStonks().onEach { db.stockDao().insertOrUpdate(it) }
+            .launchIn(viewModelScope)
     }
 
 }
